872600580ab2c9733f1dc53fb50f6e6363a7542f
Razvan Vancea <razvanvancea94@gmail.com>
2026-05-13 16:02:11 +0300

    update visual tests

--- tests/visual/visual-thank-you.spec.ts
+++ tests/visual/visual-thank-you.spec.ts
@@ -22,22 +22,4 @@ test.describe('Visual tests test suite', () => {
       fullPage: true,
     });
   });
-
-  test('visual test: validate the thank you page design', async ({
-    registrationFormPage,
-    page,
-  }) => {
-    await registrationFormPage.completeRegistration(
-      '68901',
-      'john doe',
-      'jd@gmail.com',
-      '0765666666'
-    );
-
-    await expect(page.getByRole('heading')).toContainText('Thank you!');
-
-    await expect(page).toHaveScreenshot('thank-you-page.png', {
-      fullPage: true,
-    });
-  });
 });
